dcabbea57219550120c8665b3c97da4979d47e8d
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-01-27 17:32:33 +0100

    cards added inside the container

--- src/components/card/card.stories.js
+++ src/components/card/card.stories.js
@@ -2,6 +2,7 @@ import "./card.css";
 import cardThai from "./card-thai.html";
 import cardItalian from "./card-italian.html";
 import cardCake from "./card-cake.html";
+import cardContainer from "./card-container.html";
 
 export default {
   title: "Components/Card",
@@ -11,3 +12,5 @@ export default {
 export const thai = () => cardThai;
 export const italian = () => cardItalian;
 export const cake = () => cardCake;
+export const container = () => cardContainer;
+container.parameters = { layout: "fullscreen" };
