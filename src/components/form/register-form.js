8dcd6b2c9f5d79fd957b77078e957d555f2586f6
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-02-01 20:27:21 +0100

    add components

--- src/components/form/register-form.js
+++ src/components/form/register-form.js
@@ -1,5 +1,30 @@
+import { doc } from "prettier";
+
 export function createRegisterForm() {
-  return `
+  const form = document.createElement("form");
+  form.className = "form";
+
+  const text = document.write("p");
+  text.paragraph = "please click here";
+
+  const button = document.createElement("button");
+  button.innerText = "Register!";
+
+  function register() {
+    alert("Successfully registered!");
+  }
+
+  button.addEventListener("click", register);
+
+  const title = document.createElement("h2");
+  title.InnerText = "Reset Password";
+
+  form.append(title, button);
+
+  return form;
+}
+
+/* 
         <form class="form">
           <h2>Reset Password</h2>
           <p>Please enter your email to receive a link to create a new password via email</p>
@@ -7,4 +32,4 @@ export function createRegisterForm() {
           <input type="submit" />
         </form>
       `;
-}
+      */
